0e4b2a491aa5f4fbbb2525ce06e234ad577982a7
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2024-02-06 21:35:15 +0100

    fix error in test file

--- src/app/feature/console/services/Commands/clear-command.spec.ts
+++ src/app/feature/console/services/Commands/clear-command.spec.ts
@@ -1,7 +1,9 @@
 import { ClearCommand } from './clear-command';
+import {ConsoleAction} from "../../models/console-action";
+import {BehaviorSubject} from "rxjs";
 
 describe('ClearCommand', () => {
   it('should create an instance', () => {
-    expect(new ClearCommand()).toBeTruthy();
+    expect(new ClearCommand(new BehaviorSubject<ConsoleAction[]>([]))).toBeTruthy();
   });
 });
