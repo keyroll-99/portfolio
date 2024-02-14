d7a25f593c086d22a48a2a34b57371608f26d689
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2024-02-14 19:51:24 +0100

    adding support query param (work-in-progress)

--- src/app/feature/console/pages/app-console/app-console.component.ts
+++ src/app/feature/console/pages/app-console/app-console.component.ts
@@ -4,6 +4,7 @@ import {ConsoleService} from "../../services/console.service";
 import {ConsoleAction} from "../../models/console-action";
 import {NgForOf} from "@angular/common";
 import {CommandResultComponent} from "../../components/command-result/command-result.component";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-app-console',
@@ -20,7 +21,18 @@ export class AppConsoleComponent implements OnInit{
   consoleHistory: ConsoleAction[] = [];
   @ViewChild(ConsoleInputComponent) input!: ElementRef<ConsoleInputComponent>;
 
-  constructor(private consoleService: ConsoleService) { }
+  constructor(private consoleService: ConsoleService, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((param: any) => {
+      this.route.queryParams.subscribe(params => {
+
+        if (params['query']) {
+          this.consoleService.runCommand(params['query']);
+        }
+      });
+
+    })
+
+  }
 
   ngOnInit(): void {
     setTimeout(() => this.focusOnConsoleInput(), 0);
